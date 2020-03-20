b50d01b2368f24265aa9bd1eb4b06904206df03a
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-19 21:45:10 -0400

    made a runprompt function

--- employTrack.js
+++ employTrack.js
@@ -17,3 +17,51 @@ connection.connect(function(err) {
     runPrompt();
 });
 
+function runPrompt() {
+    inquirer.prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all employees by Department",
+            "View all employees by Manager",
+            "View all roles",
+            "Add employee",
+            "Remove employee",
+            "Update employee roles",
+        ]
+    })
+    .then(function(answer) {
+        switch(answer.action) {
+            case "View all employees":
+                allEmployees();
+                break;
+
+            case "View all employees by Department":
+                allEmByDept();
+                break;
+
+            case "View all employees by Manager":
+                allEmByMan();
+                break;
+
+            case "View all roles":
+                allRoles();
+                break;
+
+            case "Add employee":
+                addEmployee();
+                break;
+
+            case "Remove employee":
+                remEmployee();
+                break;
+
+            case "Update employee roles":
+                updateEmployee();
+                break;
+        }
+    });
+}
+
