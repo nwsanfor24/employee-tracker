244be27cedaf6535fd6232198dfb79d2ba3cdceb
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-19 20:12:53 -0400

    Made changes to seed.sql

---
+++ seed.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL AUTO_INCREMENT,
+);
