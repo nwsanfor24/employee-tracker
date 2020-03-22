a1e8d63e33b8c2a861c1ca779f662d1cf30c93fc
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-21 21:00:06 -0400

    made some seeds

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Scott", "Granger", "Architect", "Denice");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Harry", "Potter", "Wizard", "Ginny Weasely");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Aragorn", "Arathorn", "Ranger", "Denethor");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Gimli", "Gloin", "Warrior", "Legolas");
